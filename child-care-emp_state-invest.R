ad9057e79da21daee8a32433a0e24cff13eedb51
Laura Valle-Gutierrez <valle-gutierrez@tcf.org>
2024-11-18 12:39:05 -0500

    Creating file and project for analysis of labor force levels in states that invested in child care workforce.

---
+++ child-care-emp_state-invest.R
@@ -0,0 +1,8 @@
+# File name: child-care-emp_state-invest.R
+# Author: Laura Valle-Gutierrez
+# Date: November 2024
+# Desc: Analyzing state child care employment levels by significant state investment
+
+library (tidyverse)
+library (ipumsr)
+library (usethis) 
